12caf856a4e247b7381f2907f5d1e1cf95d98e06
Thomas Kierzkowski <tjkierzkowski@gmail.com>
2012-01-20 02:33:07 -0500

    Added mouse movement controls using BasicControls.py. Still need to fix the walk procedure, added that to TODO

---
+++ BasicControls.py
@@ -0,0 +1,48 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.DirectObject import DirectObject
+from direct.task import Task
+########################################
+##  Handles Mouse Input and Controls  ##
+########################################
+class BasicControls(ShowBase):
+    def __init__(self):
+        self.mouseChangeX = 0
+        self.mouseChangeY = 0
+        self.windowSizeX = base.win.getXSize()
+        self.windowSizeY = base.win.getYSize()
+        self.centerX = self.windowSizeX / 2
+        self.centerY = self.windowSizeY / 2
+        self.H = base.camera.getH()
+        self.P = base.camera.getP()
+        self.pos = base.camera.getPos()
+        self.sensitivity = .5
+        self.speed = .2
+       
+    def movement(self, task):
+        mouse = base.win.getPointer(0)
+        x = mouse.getX()
+        y = mouse.getY()
+        if base.win.movePointer(0, self.centerX, self.centerY):
+            self.mouseChangeX = self.centerX - x
+            self.mouseChangeY = self.centerY - y
+            self.H += self.mouseChangeX * self.sensitivity
+            self.P += self.mouseChangeY * self.sensitivity
+            base.camera.setHpr(self.H , self.P, 0)
+           
+        self.accept("w", self.walk)
+        return Task.cont
+
+    def startMovement(self):
+        base.win.movePointer(0, self.centerX, self.centerY)
+        taskMgr.add(self.movement, 'movement')
+
+    def stopMovement(self):
+        taskMgr.remove('movement')
+       
+    def walk(self):
+        dir = base.camera.getNetTransform().getMat().getRow3(1)
+        dir.setZ(0)
+        dir.normalize()
+        self.pos += dir * self.speed
+        base.camera.setPos(self.pos)
+       
--- MainGame.py
+++ MainGame.py
@@ -2,28 +2,38 @@ import sys, os
 from pandac.PandaModules import Filename
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from BasicControls import BasicControls
 
 class MainGame(ShowBase):
 
 	def __init__(self):
 		ShowBase.__init__(self)
 		
-		#TODO - find a way to load local directory models
 		#Get the location of MainGame.py
 		projectdir = os.path.abspath(sys.path[0])
 		#convert it to Panda's unix-style notation.
 		projectdir = Filename.fromOsSpecific(projectdir).getFullpath()
-		#Load the level/environment model
-		self.environ = self.loader.loadModel(projectdir + "/models/ThePlayground")
-		#self.environ = self.loader.loadModel("models/ThePlayground.egg")
 		
-		#Reparent the model to the root of the Scenegraph, render
-		#this allows for the object to actually appear in the window
+		#TODO - Load the level model here
+		#base.wireframeOn()
+		#self.environ = self.loader.loadModel(projectdir + "/models/levels/ThePlayground")
+		#Load the default environment model from the panda Tutorial
+		self.environ = self.loader.loadModel(projectdir + "/models/levels/environment")
+		
+		#Reparent the model to the root of the Scenegraph, aka the "render" node
+		#this allows for the object to actually appear on screen
 		self.environ.reparentTo(self.render)
 		
 		#Scale and position the level/environment model
 		self.environ.setScale(0.25, 0.25, 0.25)
 		self.environ.setPos(-8, 42, 0)
+		
+		#TODO add camera controls to a function that can be added to the taskMgr
+		#Handle camera controls here
+		controls = BasicControls()
+		controls.startMovement()
+		self.taskMgr.add(controls.movement, 'movement')
+		
 	
 app = MainGame()
 app.run()
